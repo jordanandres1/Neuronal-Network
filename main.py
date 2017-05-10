4425b55d1cf176f1af7fad194a48f156304a7db4
Jason <jasoncc95@gmail.com>
2017-05-09 22:37:41 -0600

    Probando MNIST

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from scipy.optimize import fmin # funcion de optimizacion
 from nnFuncionCosto import nnFuncionCosto
 from scipy.special import expit # funcion sigmoide ej: expit(9)
 from scipy.stats import logistic # funcion sigmoide gradiente ej: logistic.pdf(np.array([[2, 4, 6], [7, 8, 9]]))
+from mnist import MNIST
+import random
 
 # Este metodo carga las 10000 imagenes
 def leerArchivo():
@@ -16,15 +18,29 @@ def main():
 	num_labels = 10
 
 	print 'Cargando y visualizando datos\n'
-	training_set = leerArchivo()
-	m = len(training_set)
-	print m
+	#training_set = leerArchivo()
+	#m = len(training_set)
 
-	print '\nFeedforward usando Redes Neuronales...\n'
-	lambdaP = 0
-	nn_params = np.array([[0], [0]])
-	X  = training_set
+	#print '\nFeedforward usando Redes Neuronales...\n'
+	#lambdaP = 0
+	#nn_params = np.array([[0], [0]])
+	#X  = training_set
 	#J = nnFuncionCosto(nn_params, input_layer_size, hidden_layer_size, num_labels, X, y, lambdaP);
+
+
+	
+
+	mndata = MNIST('datos')
+
+	images, labels = mndata.load_testing()
+
+	index = random.randrange(0, len(images))  # choose an index ;-)
+	
+	print mndata.display(images[0])
+	print
+	print len(images)
+	print len(images[0])
+	print labels[0]
 
 main()
 
