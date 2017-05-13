48215dc01158794d7e3425e4e5181aea2a951d29
Jordan Andrés Villalobos Campos <jordanandres1@hotmail.com>
2017-05-12 21:03:22 -0600

    Arreglo del nn_params

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
+import sys
 import numpy as np
+import idx2numpy
 from scipy.optimize import fmin # funcion de optimizacion
 from nnFuncionCosto import nnFuncionCosto
 from scipy.special import expit # funcion sigmoide ej: expit(9)
 from scipy.stats import logistic # funcion sigmoide gradiente ej: logistic.pdf(np.array([[2, 4, 6], [7, 8, 9]]))
-from mnist import MNIST
 import random
+# from mnist import MNIST
 
-
-def leerTrainingSet(mndata):
-	images, labels = mndata.load_training()
-	x_training = np.array(images)
-	y_training = np.array(labels)
+# Este metodo carga las 60000 imagenes
+def leerTrainingSet():
+	x_training = idx2numpy.convert_from_file('datos/train-images-idx3-ubyte') # Las imagenes son 60000 cada una de 28x28
+	y_training = idx2numpy.convert_from_file('datos/train-labels-idx1-ubyte')
 	return (x_training, y_training)
 
+def printMatrix(matrix):
+	for i in range(len(matrix)):
+		for j in range(len(matrix[0])):
+			if matrix[i][j] == 0:
+				sys.stdout.write('.')
+			else:
+				sys.stdout.write('#')
+		print
+	print
 
 def main():
 	input_layer_size  = 784
@@ -20,22 +30,28 @@ def main():
 	num_labels = 10
 
 	print 'Cargando y visualizando datos\n'
-	mndata = MNIST('datos')
-	(x_training, y_training) = leerTrainingSet(mndata)
 
-	print mndata.display(x_training[0])
-	print
-	print mndata.display(x_training[1])
-	print
-	print mndata.display(x_training[2])
-	print
-	print mndata.display(x_training[3])
-	print
-	print mndata.display(x_training[4])
+	(x_training, y_training) = leerTrainingSet()
 
 	print '\nFeedforward usando Redes Neuronales...\n'
 	lambdaP = 0
-	nn_params = np.array([[0], [0]])
+	theta1 = np.random.random((hidden_layer_size, input_layer_size + 1))	
+	theta2 = np.random.random((num_labels, hidden_layer_size + 1))
+
+	#print theta1
+	#print theta2
+
+	vectorTheta1 = theta1.flatten(1)
+	vectorTheta2 = theta2.flatten(1)
+
+	#print vectorTheta1.shape
+	#print vectorTheta2.shape
+
+
+	nn_params = np.concatenate((vectorTheta1, vectorTheta2), axis=0)
+	#print nn_params.shape
+
+
 	J = nnFuncionCosto(nn_params, input_layer_size, hidden_layer_size, num_labels, x_training, y_training, lambdaP);
 
 main()
--- nnFuncionCosto.py
+++ nnFuncionCosto.py
@@ -1,8 +1,17 @@
 import numpy as np
 
 def nnFuncionCosto(nn_params, input_layer_size, hidden_layer_size, num_labels, X, y, lambdaP):
-	#Theta1 = np.reshape(nn_params[0:hidden_layer_size * (input_layer_size + 1)], hidden_layer_size, (input_layer_size + 1));
-	#Theta2 = np.reshape(nn_params[(1 + (hidden_layer_size * (input_layer_size + 1))):], num_labels, (hidden_layer_size + 1)); 
+	
+	theta1 = np.reshape(nn_params[0:hidden_layer_size * (input_layer_size + 1)], (hidden_layer_size, (input_layer_size + 1)))
+	theta2 = np.reshape(nn_params[((hidden_layer_size * (input_layer_size + 1))):], (num_labels, (hidden_layer_size + 1)));
+	#print theta1
+	#print nn_params[(1 + (hidden_layer_size * (input_layer_size + 1))):]
+
+	#theta1 = np.reshape(nn_params[0:hidden_layer_size * (input_layer_size + 1)], hidden_layer_size, (input_layer_size + 1));
+	#theta2 = np.reshape(nn_params[(1 + (hidden_layer_size * (input_layer_size + 1))):], num_labels, (hidden_layer_size + 1));
+
+	#print theta1
+	#print theta2
 
 	m = len(X)
 
@@ -10,7 +19,14 @@ def nnFuncionCosto(nn_params, input_layer_size, hidden_layer_size, num_labels, X
 	Theta1_grad = np.zeros(X.shape)
 	Theta2_grad = np.zeros(y.shape)
 
-	'''
+	unos = np.ones((m, 1))
+
+	print unos.shape
+	print X.shape
+
+	#a1 = np.concatenate((unos, X))
+
+	'''t
 	#Se procede a realizar el feedforward
 	a1 = np.concatenate(np.ones(shape=(m, 1)), X)
 	z1 = a1*Theta1.transpose()
